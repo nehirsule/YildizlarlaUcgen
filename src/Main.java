4ad098bf0bb09d59b7a823b875c18f0339225b47
Şule Nehir ARAÇ <nehir.2706@gmail.com>
2024-03-08 11:20:31 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Tabanı oluşturacak yıldız sayısını giriniz: ");
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) { //istenilen satır sayısı kadar satır elde edilmesi
+
+            for (int k = 1; k <= (n - i); k++) { //boşluk sayısı hesaplanmas
+                System.out.print(" ");
+            }
+            for(int j=1; j<=(2*i)-1; j++){ //yıldız bastırdık
+                System.out.print("*");
+            }
+            System.out.println(" "); // her döngüdenden sonra alt satıra gecmemız gerekioyor (ln)
+        }
+
+    }
 }
